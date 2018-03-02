0d56866f78372b7ab112f3460c30c570ae268edb
NomisGnos <NomisGnos@hotmail.com>
2018-03-02 12:07:09 -0500

    Update and rename serverinfo.js to serverinfo_example.js
    added appid here

---
+++ AWS Files - Lambda/serverinfo_example.js
@@ -0,0 +1,3 @@
+exports.host = "your-home-server.no-ip.com"; //Change this YOUR public IP - must enable port forwarding. Place within quotation marks (leave the quotes)
+exports.port = 1234; //Change this to YOUR forwarded port - no quotation marks
+exports.appID = "" //replace this with your app ID to make use of APP_ID verification
